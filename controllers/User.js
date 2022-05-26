fe680f53aed4ed87bdb715f80c1ec8ca2ee6ddc1
Kenk <kenghin_lim@hotmail.com>
2022-05-26 18:33:12 +0800

    feat(Bind User): Update Logic

--- controllers/User.js
+++ controllers/User.js
@@ -1,5 +1,6 @@
 const User = require("../models/USers.model");
 const ethers = require("ethers");
+const axios = require("axios");
 
 function verifySignature(message, signature, address) {
   return address === ethers.utils.verifyMessage(message, signature);
@@ -47,4 +48,39 @@ exports.bindAddress = async (req, res, next) => {
     message: `Address ${address_to_bind} binded to ${address}`,
   });
 };
-exports.getNfts = async (req, res, next) => {};
+exports.getNfts = async (req, res, next) => {
+  let { address } = req.query;
+  console.log("🚀 | exports.getNfts= | address", address);
+
+  // Check if valid address
+  try {
+    address = ethers.utils.getAddress(address);
+  } catch {
+    return res.status(400).json({
+      message: "Invalid address",
+    });
+  }
+  // const chain = process.env.NODE_ENV === "prod" ? "1" : "4"; // TODO: TATUM DONT SUPPORT RINKEBY
+  // const TATUM_API_KEY =  process.env.NODE_ENV === "prod"
+  // ? process.env.TATUM_PROD_API_KEY
+  // : process.env.TATUM_DEV_API_KEY
+  const chain = "ETH";
+  const TATUM_API_KEY = process.env.TATUM_PROD_API_KEY;
+
+  // TODO: Add Solana support in the future
+  const resp = await axios.get(
+    `https://api-us-west1.tatum.io/v3/nft/address/balance/${chain}/${address}`,
+    {
+      method: "GET",
+      headers: {
+        "x-api-key": TATUM_API_KEY,
+      },
+    }
+  );
+  console.log("🚀 | exports.getNfts= | resp", resp.data);
+
+  return res.status(200).json({
+    message: "Nfts retrieved",
+    nfts: resp.data.data,
+  });
+};
