888ccecfe9090d9a134b3d67f3a5cb1b10ee0cd1
Kenk <kenghin_lim@hotmail.com>
2022-05-26 18:32:44 +0800

    test(Bind User): Update Test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,29 +1,34 @@
+const request = require("supertest");
 const app = require("../server");
 
 require("dotenv").config();
+
+jest.setTimeout(30000);
 
 describe("User", () => {
   beforeAll(async () => {});
 
   afterAll(async () => {
+    // Close DB Collection
+    await app.db.connection.close();
+
     // Close Express Server
     app.server.close();
 
     // Remove all data that was added during tests
-
-    // Close DB Collection
-    await app.db.connection.close();
   });
 
-  test("Bind Account", () => {
-    const response = request(app).get("/api/v1/user/bind");
+  test("Bind Account", async () => {
+    const response = await request(app).post("/api/v1/user/bind");
 
     expect(response.status).toEqual(200);
     expect(response.body.data.message).toEqual("TODO");
   });
 
-  test("Get NFTS", () => {
-    const response = request(app).get("/api/v1/user/nfts");
+  test("Get NFTS", async () => {
+    const response = await request(app).get(
+      "/api/v1/user/nfts?address=0xA63dDdB69E6e470Bf3d236B434EF80a213B998A7"
+    );
 
     expect(response.status).toEqual(200);
     expect(response.body.data.message).toEqual("TODO");
