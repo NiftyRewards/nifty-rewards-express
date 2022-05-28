663a8b44cad785ed9805895c8908a32f3ed670b5
Kenk <kenghin_lim@hotmail.com>
2022-05-28 12:06:28 +0800

    feat(Cache Collection): Add Collection Caching

--- models/Collections.model.js
+++ models/Collections.model.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const collectionsSchema = new mongoose.Schema({
   collection_address: { type: String, required: true },
-  collection_name: { type: String, required: true },
+  chain: { type: String, required: true },
+  collectionIdentifier: { type: String, required: true },
+  campaigns: { type: [String], required: true },
+  cache: { type: Object, required: true },
+  totalSupply: { type: Number },
+  cache_last_updated: { type: Date },
 });
 
 module.exports = mongoose.model("Collection", collectionsSchema);
---
+++ tests/collection.test.js
@@ -0,0 +1,43 @@
+const request = require("supertest");
+const app = require("../server");
+const mongoose = require("mongoose");
+
+const CollectionModel = require("../models/Collections.model");
+require("dotenv").config();
+
+jest.setTimeout(3000000);
+
+const AZUKI_CONTRACT_ADDRESS = "0xED5AF388653567Af2F388E6224dC7C4b3241C544";
+const MERCHANT_ADDRESS = "0xA63dDdB69E6e470Bf3d236B434EF80a213B998A7";
+
+describe("Campaign", () => {
+  beforeAll(async () => {
+    // Remove all data that was added during tests
+    await CollectionModel.deleteMany({});
+  });
+
+  afterAll(async () => {
+    // Remove all data that was added during tests
+    // await CampaignsModel.deleteMany({});
+
+    // Close DB Collection
+    // await app.db.connection.close();
+    mongoose.connection.close();
+
+    // Close Express Server
+    app.server.close();
+  });
+
+  test("Cache Campaign", async () => {
+    const response = await request(app)
+      .post("/api/v1/collection/cache")
+      .set("Content-Type", "application/json")
+      .send({
+        collectionAddress: AZUKI_CONTRACT_ADDRESS,
+        chainId: "1",
+      });
+
+    expect(response.status).toEqual(200);
+    expect(response._body.message).toEqual("Collection Cached");
+  });
+});
