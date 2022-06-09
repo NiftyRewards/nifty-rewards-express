c4233aeb4f064e73c9fa54106247a9787598fea3
Kenk <kenghin_lim@hotmail.com>
2022-06-09 17:42:04 +0800

    feat: Update Database connection

--- config/db.js
+++ config/db.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 let MONGODB_URI;
-const DB_DOMAIN_PROD = process.env.DB_DOMAIN_PROD;
-const DB_DOMAIN_DEV = process.env.DB_DOMAIN_DEV;
-const DATABASE_NAME = process.env.DATABASE_NAME
-  ? process.env.DATABASE_NAME
-  : "NiftyRewards";
+const DB_DOMAIN = process.env.DB_DOMAIN;
+const DATABASE_NAME_DEV = process.env.DATABASE_NAME_DEV;
+const DATABASE_NAME_PROD = process.env.DATABASE_NAME_PROD;
+
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
@@ -22,10 +21,10 @@ const connectDB = () => {
     MONGODB_URI = `mongodb://localhost:27017/${DATABASE_NAME}`;
   } else if (process.env.NODE_ENV == "dev") {
     console.log("DEV DB...");
-    MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_DOMAIN_DEV}/${DATABASE_NAME}`;
+    MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_DOMAIN}/${DATABASE_NAME_DEV}`;
   } else if (process.env.NODE_ENV == "prod") {
     console.log("PROD DB...");
-    MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_DOMAIN_PROD}/${DATABASE_NAME}`;
+    MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_DOMAIN}/${DATABASE_NAME_PROD}`;
   } else {
     console.log("ENV NOT SET!");
     process.exit(1);
